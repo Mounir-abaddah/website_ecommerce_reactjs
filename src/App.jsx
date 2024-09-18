408eadd3163ccc7140253d1c3ea4ca9b88f8bba1
Mounir abaddah <mounirabaddah6@gmail.com>
2024-09-18 20:09:46 +0100

    finale

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/collection" element={<Collection />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/place-order" element={<PlaceOrder />} />
           <Route path="/orders" element={<Orders />} />
         </Routes>
